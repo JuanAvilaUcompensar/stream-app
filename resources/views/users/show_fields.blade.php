b627ad60813b3c2380f9bb38cb958a32190b9360
Juan Windows11 <jmanuelavilaperez@gmail.com>
2023-10-25 23:18:23 -0500

    base de datos agragada

--- resources/views/users/show_fields.blade.php
+++ resources/views/users/show_fields.blade.php
@@ -1,9 +1,7 @@
-<!-- Roles Id Field -->
-<div class="col-sm-12">
-    {!! Form::label('roles_id', 'Roles Id:') !!}
-    <p>
-        <a href="{{ route('roles.show', $user->roles['id']) }}" class="btn btn-outline-info"> {{$user->roles['name']}} </a>
-    </p>
+<!-- Name Field -->
+<div class="col-sm-12" style="margin-bottom: 1%;">
+    <h6><strong>Rol de este usuario:</strong></h6>
+    <a href="/roles/{{$user->roles_id}}"><button type="button" class="btn btn-outline-primary">{{ $user->role_name[0]['name'] }}</button></a>
 </div>
 
 <!-- Name Field -->
@@ -18,38 +16,92 @@
     <p>{{ $user->email }}</p>
 </div>
 
-<!-- Email Verified At Field -->
-<div class="col-sm-12">
-    {!! Form::label('email_verified_at', 'Email Verified At:') !!}
-    <p>{{ $user->email_verified_at }}</p>
-</div>
 
-<!-- Password Field -->
-<div class="col-sm-12">
+<h1>TRANSACCIONES DE ESTE USUARIO</h1>
 
-    <table>
-        <thead>
+<div class="col-sm-12">
+    <table class="table">
+        <thead class="table-dark">
             <tr>
-                <th>sdf</th>
-                <th>sdf</th>
-                <th>sdf</th>
-                <th>sdf</th>
+                <th scope="col">Transactions Id</th>
+                <th scope="col">Producto</th>
+                <th scope="col">Amount</th>
+                <th scope="col">payment_method</th>
+                <th scope="col">status</th>
             </tr>
         </thead>
-        
-       <tbody>
+        <tbody>
+            @php
+                $totalAmount = 0;  // Inicializamos la variable para almacenar el total
+            @endphp
+            @foreach ($user->transactions as $transaction)
+                <tr>
+                    <td>{{ $transaction->id }}</td>
+                    <td>
+                        <a href="/qrcodes/{{$transaction->qrcode_id}}">
+                            {{ $transaction->product['product_name']}}
+                        </a>
+                    <img src="../{{ $transaction->product['product_url_image_path']}}" width="100px" />
+                    </td>
+                    <td>{{ $transaction->amount }}</td>
+                    <td>{{ $transaction->payment_method }}</td>
+                    <td>{{ $transaction->status }}</td>
+                </tr>
+                @php
+                    $totalAmount += $transaction->amount;  // Sumamos el monto de la transacción al total
+                @endphp
+            @endforeach
+        </tbody>
+        <tfoot>
             <tr>
-                @foreach($user->transactions as $transaction)
-                    <th>{{ $transaction->amount}}</th> 
-                @endforeach        
+                <td colspan="2"></td>
+                <td>Total: ${{ $totalAmount }}</td>
+                <td></td>
+                <td></td>
             </tr>
-       </tbody>
+        </tfoot>
     </table>
 </div>
 
-<!-- Remember Token Field -->
-<div class="col-sm-12">
-    {!! Form::label('remember_token', 'Remember Token:') !!}
-    <p>{{ $user->remember_token }}</p>
-</div>
 
+<h1>PRODUCTOS DE ESTE USUARIO</h1>
+
+<div class="col-sm-12">
+    <table class="table">
+        <thead class="table-dark">
+            <tr>
+                <th scope="col ">Producto Id</th>
+                <th scope="col">Producto</th>
+                <th scope="col">amout</th>
+            </tr>
+        </thead>
+        <tbody>
+            @php
+                $totalAmount = 0;  // Inicializamos la variable para almacenar el total
+            @endphp
+            @foreach ($user->qrcodes as $qrcode)
+                <tr>
+                    <td>{{ $qrcode->id }}</td>
+                    <td>
+                        <a href="/qrcodes/{{$qrcode->id}}">
+                            {{ $qrcode->product_name }}
+                        </a>
+                        <img src="../{{$qrcode->product_url_image_path}}" width="100px" />
+                    </td>
+                    <td>{{ $qrcode->amount }}</td>
+                </tr>
+                @php
+                    $totalAmount += $qrcode->amount;  // Sumamos el monto de la transacción al total
+                @endphp
+            @endforeach
+        </tbody>
+        <tfoot>
+            <tr>
+                <td colspan="2"></td>
+                <td>Total: ${{ $totalAmount }}</td>
+                <td></td>
+                <td></td>
+            </tr>
+        </tfoot>
+    </table>
+</div>
